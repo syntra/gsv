7fa1f598c56ed486f20188278e37774f38e3001e
Garet McKinley <self@garet.io>
2018-09-18 06:05:16 -0500

    tests: add test for creating new blog

--- test/commands/new/blog.test.ts
+++ test/commands/new/blog.test.ts
@@ -1,4 +1,6 @@
 import { expect, test } from "@oclif/test";
+import * as rimraf from "rimraf";
+import { existsSync } from "fs";
 import errors from "../../../src/config/errors";
 
 describe("new:blog", () => {
@@ -12,4 +14,11 @@ describe("new:blog", () => {
     .command(["new:blog", "path", "--starter", "invalid-starter"])
     .exit(errors.new.blog.invalid_starter().exit)
     .it("fails when invalid starter is set");
+  test
+    .stdout()
+    .command(["new:blog", "test-blog"])
+    .it("creates new blog", ctx => {
+      expect(existsSync("test-blog")).to.be.true;
+      rimraf.sync("test-blog");
+    });
 });
