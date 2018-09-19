63288720b8f87190d6952431dcb79d67ec0fabb2
Garet McKinley <self@garet.io>
2018-09-19 07:26:02 -0500

    tests: add utils/gsvrc.write() suite

---
+++ test/utils/gsvrc/write.test.ts
@@ -0,0 +1,34 @@
+import { expect } from "@oclif/test";
+import { existsSync, unlinkSync } from "fs";
+
+import gsvrc from "../../../src/utils/gsvrc";
+
+describe("utils/gsvrc.write()", () => {
+  it("writes config", async () => {
+    await gsvrc.write({
+      title: "Test Blog",
+      url: "https://syntra.io",
+      author: {
+        name: "Garet McKinley",
+        email: "hi@garet.io",
+      },
+    });
+    expect(existsSync(".gsvrc")).to.be.true;
+    unlinkSync(".gsvrc");
+  });
+
+  it("overrides when forced", async () => {
+    const config = {
+      title: "Test Blog",
+      url: "https://syntra.io",
+      author: {
+        name: "Garet McKinley",
+        email: "hi@garet.io",
+      },
+    };
+    await gsvrc.write(config);
+    await gsvrc.write(config, true);
+    expect(existsSync(".gsvrc")).to.be.true;
+    unlinkSync(".gsvrc");
+  });
+});
