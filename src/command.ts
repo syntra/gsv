a4151edc3a687e015913ce29c39b5583948e7e90
Garet McKinley <self@garet.io>
2018-09-19 05:05:10 -0500

    fix: command.log() formatting

--- src/command.ts
+++ src/command.ts
@@ -49,6 +49,6 @@ export default abstract class Command extends OCommand {
     // tslint:disable-next-line strict-type-predicates
     message = typeof message === "string" ? message : inspect(message);
     const prefix = chalk.cyan(" ›  ");
-    process.stdout.write(format(prefix, message, ...args).trim() + "\n");
+    process.stdout.write(prefix + format(message, ...args).trim() + "\n");
   }
 }
