fa752ea65c8912aafaebc1181917dd71f66f8c18
Garet McKinley <self@garet.io>
2018-09-19 04:00:16 -0500

    tests: add utils/git test suite

---
+++ test/__mock_repo/README.md
@@ -0,0 +1,3 @@
+# mock_repo
+
+this is just a repo that is used by the git clone tests
---
+++ test/utils/git.test.ts
@@ -0,0 +1,25 @@
+import { expect } from "@oclif/test";
+import * as rimraf from "rimraf";
+import { existsSync, renameSync } from "fs";
+
+import git from "../../src/utils/git";
+
+describe("utils/git", () => {
+  before(() => {
+    const repo = "test/__mock_repo";
+    renameSync(`${repo}/git`, `${repo}/.git`);
+  });
+
+  after(() => {
+    const repo = "test/__mock_repo";
+    renameSync(`${repo}/.git`, `${repo}/git`);
+  });
+
+  it("clones repo", async () => {
+    const repo = "test/__mock_repo";
+    const dest = "__test_clone";
+    await git.clone(repo, dest);
+    expect(existsSync(dest)).to.be.true;
+    rimraf.sync(dest);
+  });
+});
