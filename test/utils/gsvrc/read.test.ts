44391abf007ed99d1b711433d136eb68494ecbf7
Garet McKinley <self@garet.io>
2018-09-20 01:29:57 -0500

    tests: expect error when reading nonexistent gsvrc

--- test/utils/gsvrc/read.test.ts
+++ test/utils/gsvrc/read.test.ts
@@ -1,12 +1,16 @@
 import { expect } from "@oclif/test";
 import { writeFileSync, existsSync, unlinkSync } from "fs";
 import YAML from "yaml";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 
 import gsvrc from "../../../src/utils/gsvrc";
 import { write } from "fs-extra";
 
 describe("utils/gsvrc.read()", () => {
-  before(() => {
+  it("reads config", async () => {
     writeFileSync(
       ".gsvrc",
       YAML.stringify({
@@ -20,18 +24,17 @@ describe("utils/gsvrc.read()", () => {
         ],
       })
     );
-  });
-
-  after(() => {
-    unlinkSync(".gsvrc");
-  });
-
-  it("reads config", async () => {
     const config = await gsvrc.read();
     expect(config.title).to.equal("Test Blog");
     expect(config.url).to.equal("https://syntra.io");
     expect(config.author.length).to.equal(1);
     expect(config.author[0].name).to.equal("Garet McKinley");
     expect(config.author[0].email).to.equal("hi@garet.io");
+
+    unlinkSync(".gsvrc");
+  });
+
+  it("returns error if config doesn't exist", async () => {
+    await chai.expect(gsvrc.read()).to.eventually.be.rejected;
   });
 });
